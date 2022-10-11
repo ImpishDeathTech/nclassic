042d7d01a2587a169c7186ccb739803daa4e0ee1
Christopher Stephen Rafuse <81781310+ImpishDeathTech@users.noreply.github.com>
2022-10-11 15:19:45 +0000

    Update angle.lua

--- example/angle.lua
+++ example/angle.lua
@@ -1,3 +1,13 @@
+
+--
+-- angle.lua
+-- 
+-- Copyright (c) 2022 Christopher Stephen Rafuse, ImpishDeathTech
+--
+-- This module is free software; you can redistribute it and/or modify it under
+-- the terms of the MIT license. See LICENSE for details.
+--
+
 local Object = require 'nclassic'
 
 local Angle = Object:extend('angle')
