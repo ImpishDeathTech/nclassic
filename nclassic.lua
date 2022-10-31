003b3a89fd56db9e2a105779c29e17c90eafb930
Christopher Stephen Rafuse <81781310+ImpishDeathTech@users.noreply.github.com>
2022-10-31 15:18:42 +0000

    Update nclassic.lua

--- nclassic.lua
+++ nclassic.lua
@@ -3,8 +3,8 @@
 --
 -- based on https://github.com/rxi/classic
 -- 
--- Copyright (c) 2022 Christopher Stephen Rafuse, ImpishDeathTech
 -- Copyright (c) 2014, rxi
+-- Copyright (c) 2022 Christopher Stephen Rafuse, ImpishDeathTech
 --
 -- This module is free software; you can redistribute it and/or modify it under
 -- the terms of the MIT license. See LICENSE for details.
