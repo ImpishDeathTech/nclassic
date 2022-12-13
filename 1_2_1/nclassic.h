4c4664cf2a030cd3c2a7d8b18ef181240bb53f55
Christopher Stephen Rafuse <81781310+ImpishDeathTech@users.noreply.github.com>
2022-12-13 17:12:25 +0000

    Update nclassic.h

--- 1_2_1/nclassic.h
+++ 1_2_1/nclassic.h
@@ -1,6 +1,5 @@
 #ifndef IMPISH_DEATH_TECH_NCLASSIC_H 
 #define IMPISH_DEATH_TECH_NCLASSIC_H
-    #endif
     #define NCLASSIC_BADARGUMENT "bad argument #%d to '%s' (%s expected, got %s)"
     #define NCLASSIC_VERSION "1.2.1"
     #define NCLASSIC_LUA static const char*
@@ -14,6 +13,6 @@
         #include <lua.h>
         #include <lualib.h>
         #include <lauxlib.h>
-        
         int luaopen_nclassic(lua_State* L);
+    #endif
 #endif
